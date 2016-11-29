0a6cafcff5c8a5464ecd26670bafeb3a1fd38b00
orhanbalci <orhanbalci@gmail.com>
2016-11-29 20:48:39 +0200

    P11 added

---
+++ src/test/scala/P11FunSuite.scala
@@ -0,0 +1,10 @@
+import org.scalatest.FunSuite
+
+class P11FunSuite extends FunSuite {
+  test(
+    "Encoded modified of List('a,'a,'a,'b,'c,'c) should be List((3,'a),'b,(2,'c))") {
+    assert(
+      List((3, 'a), 'b, (2, 'c)) == P11.encodeModified(
+        List('a, 'a, 'a, 'b, 'c, 'c)))
+  }
+}
