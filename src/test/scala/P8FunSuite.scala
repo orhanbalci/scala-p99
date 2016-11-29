04870a81a62eca6419193182e78d729f64bc2632
orhanbalci@gmail.com <orhan.balci@foreks.com>
2016-11-29 10:24:11 +0300

    P8 added

---
+++ src/test/scala/P8FunSuite.scala
@@ -0,0 +1,7 @@
+import org.scalatest.FunSuite
+
+class P8FunSuite extends FunSuite {
+	test("Compressing List('a, 'a, 'a, 'a, 'b, 'c, 'c, 'a, 'a, 'd, 'e, 'e, 'e, 'e) should yield List('a, 'b, 'c, 'a, 'd, 'e)"){
+		assert(List('a, 'b, 'c, 'a, 'd, 'e) == P8.compress(List('a, 'a, 'a, 'a, 'b, 'c, 'c, 'a, 'a, 'd, 'e, 'e, 'e, 'e)))
+	}
+}
